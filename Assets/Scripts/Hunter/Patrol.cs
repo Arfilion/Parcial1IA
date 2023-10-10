9792bd318a8aff95cce1efd22c843048904fd768
RocioN01 <rooene17@gmail.com>
2023-10-10 18:14:47 -0300

    updatepatrol

--- Assets/Scripts/Hunter/Patrol.cs
+++ Assets/Scripts/Hunter/Patrol.cs
@@ -8,6 +8,7 @@ public class Patrol : State
     Hunter _hunter;
     Renderer _rend;
     public Transform _target;
+    public int wayPointObjective =0 ; //prueba
 
     public Patrol(Hunter p)
     {
@@ -21,21 +22,43 @@ public class Patrol : State
 
     public override void OnUpdate()
     {
-        Vector3 _patrolPoint=Vector3.zero;
+        Vector3 _patrolPoint = Vector3.zero;
         Vector3 distProximity = (Hunter.instance.transform.position - Hunter.instance.patrolPoints[0].transform.position);
+        //Vector3 distProximity = (Hunter.instance.transform.position - Hunter.instance.patrolPoints[0].transform.position);
         Hunter.instance.AddForce(Hunter.instance.Seek(Hunter.instance.patrolPoints[0].transform.position));
-        Hunter.instance.AddForce(Hunter.instance.Arrive(Hunter.instance.patrolPoints[0].transform.position));//se mueve hacia la posicion
+        //Hunter.instance.AddForce(Hunter.instance.Arrive(Hunter.instance.patrolPoints[0].transform.position));//se mueve hacia la posicion
         Hunter.instance.Move();
 
-        if (distProximity.magnitude < 0.15f /*&& _patrolPoint==Hunter.instance.patrolPoints[0].transform.position*/)
+        if ( Vector3.Distance(Hunter.instance.transform.position, Hunter.instance.patrolPoints[0].transform.position) <0.15f)  /*distProximity.magnitude < 0.15f && _patrolPoint==Hunter.instance.patrolPoints[0].transform.position)*/
         {
             Debug.Log("Holi");
-            distProximity= Hunter.instance.transform.position - Hunter.instance.patrolPoints[1].transform.position;
-            _patrolPoint = Hunter.instance.patrolPoints[1].transform.position;
-            Hunter.instance.AddForce(Hunter.instance.Seek(Hunter.instance.patrolPoints[1].transform.position));//se mueve hacia la posicion
-            Hunter.instance.AddForce(Hunter.instance.Arrive(Hunter.instance.patrolPoints[1].transform.position));
-            Hunter.instance.Move();
+
+            
+            wayPointObjective ++;
+            if (wayPointObjective ==1)
+            {
+                  Debug.Log(wayPointObjective);
+              //  distProximity= Hunter.instance.transform.position - Hunter.instance.patrolPoints[wayPointObjective].transform.position;
+              //
+               Hunter.instance.AddForce(Hunter.instance.Seek(Hunter.instance.patrolPoints[1].transform.position));//se mueve hacia la posicion
+               Hunter.instance.AddForce(Hunter.instance.Arrive(Hunter.instance.patrolPoints[1].transform.position));
+               Hunter.instance.Move();
+               if (Vector3.Distance(Hunter.instance.transform.position, Hunter.instance.patrolPoints[1].transform.position) < 0.15f)
+               {
+                   wayPointObjective++;
+                   distProximity = Hunter.instance.transform.position - Hunter.instance.patrolPoints[wayPointObjective].transform.position;
+                   //
+                   Hunter.instance.AddForce(Hunter.instance.Seek(Hunter.instance.patrolPoints[wayPointObjective].transform.position));//se mueve hacia la posicion
+                   Hunter.instance.AddForce(Hunter.instance.Arrive(Hunter.instance.patrolPoints[wayPointObjective].transform.position));
+                   Hunter.instance.Move();
+               }
+
+            }
+            
         }
+        
+
+        //comentado a modo prueba
         Vector2 distWaypoint = Hunter.instance.transform.position - Hunter.instance.prey.transform.position;
 
         
